73ef2a1fce33a044615f2fe82d0a0cc08070034b
Ralphie Celedon <ralphie@Ralphies-MacBook-Pro.local>
2019-05-29 13:54:16 -0700

    added forms to profile

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -1,9 +1,23 @@
 import React, { Component} from "react";
 
 class Profile extends Component {
+    state = {
+        username: "",
+        password: ""
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
     render(){
         return(
-            <h1>Profile</h1>
+            <form className="loginforms">
+            <h1>Edit Info</h1>
+                <input type="text" name="username" placeholder="Username" className="inputbox"></input><br/>
+                <input type="password" name="password" placeholder="Password" className="inputbox"></input><br/>
+                <button type="submit" className="btn">Edit</button>
+            </form>
         )
     }
 }
